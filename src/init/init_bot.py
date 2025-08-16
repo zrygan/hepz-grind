f31f38cad309eb168cc2cba6b55e65ee244461c6
JustinChing30 <justin_ethan_l_ching@dlsu.edu.ph>
2025-08-16 22:01:24 +0800

    turned on presences intent

--- src/init/init_bot.py
+++ src/init/init_bot.py
@@ -9,6 +9,7 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
+intents.presences = True
 
 bot = commands.Bot(command_prefix='#', intents=intents)
 
